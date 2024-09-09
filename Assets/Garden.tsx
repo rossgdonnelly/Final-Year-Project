550f67988ceba2c89e16af4312c0fbdc86b99d0a
Ross Donnelly <rossgdonnelly@gmail.com>
2024-09-09 16:49:59 -0400

    Add files via upload

---
+++ Assets/Garden.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Garden" tilewidth="32" tileheight="32" tilecount="432" columns="24">
+ <image source="../Exteriors/Floors_MV_TILESET_A2.png" width="768" height="576"/>
+</tileset>
---
+++ CharacterRedesigns/Goodie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Goodie" tilewidth="32" tileheight="32" tilecount="150" columns="25">
+ <image source="../../../../../Downloads/Martial Hero 2/Sprites/Idle.png" width="800" height="200"/>
+</tileset>
